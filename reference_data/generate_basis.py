741b08e9c3d3fcdc14a0f699b6d7badba20541dd
zachcran <zachcran@iastate.edu>
2021-08-06 00:26:52 +0000

    Added parsing for NWChem-formatted basis sets and updated Gaussian94 parsing slightly
    * Added command line arguments for file locations and options
    * Updated documentation for all functions involved

--- reference_data/generate_basis.py
+++ reference_data/generate_basis.py
@@ -1,27 +1,71 @@
 """
 This script will loop over a series of basis sets and write out a file that will
 fill them in.  The format of the resulting basis sets is suitable for use with
-the BasisSetExchange class
+the BasisSetExchange class.
+
+This script creates the following files:
+
+chemcache_root
+|
++---include
+|       nwx_basis_list.hpp
+|
++---src
+|   \---bases
+|           <all_basis_set_files>
+|       nwx_basis_set_manager_pimpl.cpp
 """
+
+import argparse
 import os
-import fnmatch
 import re
+import sys
+
 from generate_atomicinfo import parse_symbols
+import helper_fxns as helpers
 
 class Shell:
-    def __init__(self, l):
-        self.l = l
+    """Class representing a shell for an element.
+    """
+
+    def __init__(self, ls):
+        """Initialization function.
+
+        :param ls: List of angular momenta
+        :type ls: list of int
+        """
+
+        self.ls = ls
         self.exp=[]
         self.coefs=[]
         self.gen = 0
+
     def add_prim(self, exp, coefs):
+        """Add a primitive for the shell.
+
+        :param exp: Primitive exponent
+        :type exp: str
+        :param coefs: Primitive contraction coefficients
+        :type coefs: list of str
+        """
+
         self.exp.append(exp)
         self.coefs.append(coefs)
         self.gen = max(len(coefs), self.gen)
-    def cxxify(self, tab, f):
+
+    def cxxify(self, tab, fout):
+        """Create a C++ source representation of the shell.
+
+        :param tab: String representing a tab.
+        :type tab: str
+
+        :param fout: C++ source file opened for writing
+        :type fout: class: _io.TextIOWrapper
+        """
+        
         for i in range(self.gen):
-            l = self.l[i]
-            f.write("{}rv.add_shell(ShellType::pure, {},\n".format(tab, l))
+            l = self.ls[i]
+            fout.write("{}rv.add_shell(ShellType::pure, {},\n".format(tab, l))
             cs = "std::vector<double>{"
             es = "std::vector<double>{"
             for j,ai in enumerate(self.exp):
@@ -35,8 +79,8 @@ class Shell:
                 else:
                     cs += '}'
                     es += '}'
-            f.write("{}    {},\n".format(tab, cs))
-            f.write("{}    {});\n".format(tab, es))
+            fout.write("{}    {},\n".format(tab, cs))
+            fout.write("{}    {});\n".format(tab, es))
 
 def print_pimpl_header(f):
     f.write(
@@ -46,10 +90,10 @@ def print_pimpl_header(f):
  * this file will be lost next time generate_basis.py is run.
  */
        
-#include \"libchemist/detail_/basis_set_manager_pimpl.hpp\"
-#include \"libchemist/defaults/nwx_basis_list.hpp\"
+#include <libchemist/detail_/basis_set_manager_pimpl.hpp>
+#include \"chemcache/nwx_basis_list.hpp\"
         
-namespace libchemist::detail_ {
+namespace chemcache::basis_set::detail_ {
        
 class HardCodedBSMan : public BasisSetManagerPIMPL {
 public:
@@ -76,7 +120,7 @@ std::unique_ptr<BasisSetManagerPIMPL> nwx_default_bs() {
     return std::make_unique<HardCodedBSMan>();
 }
         
-} // namespace libchemist::detail_
+} // namespace chemcache::basis_set::detail_
 """)
 
 def print_basis_header(f, bs_name):
@@ -86,9 +130,9 @@ def print_basis_header(f, bs_name):
  * this file will be lost next time generate_basis.py is run.
  */
  
-#include \"libchemist/defaults/nwx_basis_list.hpp\"
+#include \"chemcache/nwx_basis_list.hpp\"
  
-namespace libchemist::detail_ {{
+namespace chemcache::basis_set {{
  
 Center<double> {}(std::size_t Z) {{
     switch(Z) {{         
@@ -101,8 +145,10 @@ def print_basis_list(f):
  * this file will be lost next time generate_basis.py is run.
  */
          
-#include \"libchemist/basis_set/basis_set.hpp\"
-namespace libchemist::detail_ {
+#include <libchemist/basis_set/basis_set.hpp>
+
+namespace chemcache::basis_set {
+
 """)
 
 def print_basis_footer(f):
@@ -112,7 +158,7 @@ def print_basis_footer(f):
 {}throw std::out_of_range(\"Basis not available for Z\");
 {}}}\n{}}} // end switch\n
 }} //end function
-}} //end libchemist::detail_""".format(tab*2, tab*3, tab*2, tab))
+}} //end namespace chemcache::basis_set""".format(tab*2, tab*3, tab*2, tab))
 
 def print_atom_basis(f, z, atom):
     tab = "    "
@@ -124,23 +170,57 @@ def print_atom_basis(f, z, atom):
     f.write("{}}} //End case\n".format(tab*2))
 
 def sanitize_name(bs_name):
+    """Sanitizes the basis set name. For example, replace numbers with the
+    corresponding word and hyphens, plus-signs, and other symbols with 
+    underscores.
+
+    :param bs_name: Basis set name
+    :type bs_name: str
+
+    :return: Sanitized basis set name
+    :rtype: str
+    """
+
     temp = bs_name.replace("6-", "six_dash_")
     temp = temp.replace("3-", "three_dash_")
     temp = temp.replace("-", "_dash_")
     temp = temp.replace("-", "_dash_")
     temp = temp.replace("+", "_plus_")
+
     return temp
 
 def desanitize_name(bs_name):
+    """\"Desanitizes\" the basis set name. For example, replace "_star" with
+    an asterisk character '*'.
+
+    :param bs_name: Sanitized basis set name
+    :type bs_name: str
+
+    :return: Unsanitized basis set name
+    :rtype: str
+    """
+
     temp = bs_name.replace("_star", "*")
-    temp = temp.replace("-", "-")
+    
     return temp
 
-def write_bases(out_dir, bases):
+def write_bases(inc_dir, src_dir, bases):
+    """Writes basis set data to C++ files.
+
+    :param inc_dir: Include directory for header files.
+    :type inc_dir: str
+
+    :param src_dir: Source directory for source files.
+    :type src_dir: str
+
+    :param bases: Collection of basis sets parsed from files
+    :type bases: dict
+    """
+
     tab = "    "
-    with open(os.path.join(out_dir,"nwx_basis_set_manager_pimpl.cpp"),'w') as f:
+    with open(os.path.join(src_dir,"nwx_basis_set_manager_pimpl.cpp"),'w') as f:
         print_pimpl_header(f)
-        with open(os.path.join(out_dir, "nwx_basis_list.hpp"), 'w') as g:
+        with open(os.path.join(inc_dir, "nwx_basis_list.hpp"), 'w') as g:
             print_basis_list(g)
             f.write("{}".format(tab*2))
             for bs_name, bs in sorted(bases.items()):
@@ -150,24 +230,48 @@ def write_bases(out_dir, bases):
                 f.write("return {}(Z); ".format(s_name))
                 g.write("Center<double> {}(std::size_t Z);\n".format(s_name))
                 bs_file_name = "{}.cpp".format(bs_name)
-                bs_path = os.path.join(out_dir,"bases", bs_file_name)
+                bs_path = os.path.join(src_dir,"bases", bs_file_name)
                 with open(bs_path, 'w') as h:
                     print_basis_header(h, s_name)
                     for z in sorted([int(x) for x in bs.keys()]):
                         print_atom_basis(h, z, bs[str(z)])
                     print_basis_footer(h)
                 f.write("}}\n{}else ".format(tab*2))
-            g.write("} //end namespace\n")
+            g.write("\n} // namespace chemcache::basis_set\n")
         print_pimpl_footer(f)
 
-def parse_bases(basis_sets, sym2Z, l2num):
+def parse_bases_gbs(basis_set_filenames, sym2Z, l2num):
+    """Parses basis set files from the filepaths given.
+
+    :param basis_set_filepaths: Full paths to basis set files.
+    :type basis_set_filepaths: list
+
+    :param sym2Z: Dictionary associating atomic symbols to atomic numbers
+    :type sym2Z: dict
+
+    :param l2num: Lambda function associating orbital letters with a number
+    :type l2num: lambda
+
+    :return: Collection of basis sets and the supported elements of each.
+    :rtype: dict
+    """
+
     new_atom = re.compile("^\s*\D{1,2}\s*0\s*$")
     new_shell = re.compile("^\s*[a-zA-Z]+\s*\d+\s*1.00\s*$")
-    same_shell = re.compile("^\s*(?:-?\d+.\d+(?:(E|e|D|d)(\+|-)\d\d)*\s*)+")
+    same_shell = re.compile("^\s*(?:-?\d+.\d+(?:(E|e)(\+|-)\d\d)*\s*)+")
     bases = {}
-    for bs in basis_sets:
+    for filepath in basis_set_filenames:
+        # Extract file name without extension
+        bs = os.path.splitext(os.path.basename(filepath))[0]
+
+        # Let the user know which basis set is being parsed
+        print("Parsing {}...".format(os.path.basename(filepath)), 
+              end='')
+        # Print immediately
+        sys.stdout.flush()
+
         bases[bs] = {}
-        with open(os.path.join("basis_sets","default",bs+".gbs"),'r') as f:
+        with open(filepath,'r') as f:
             atom_z = 0
             for line in f:
                 if re.search(new_atom, line):
@@ -179,23 +283,244 @@ def parse_bases(basis_sets, sym2Z, l2num):
                 elif re.search(same_shell, line):
                     prim = line.split()
                     bases[bs][atom_z][-1].add_prim(prim[0], prim[1:])
+
+        # Let the user know which basis set is being parsed
+        print("complete")
+        # Print immediately
+        sys.stdout.flush()
+
     return bases
 
+def parse_bases_nw(basis_set_filepaths, sym2Z, l2num):
+    """Parses basis set files from the filepaths given.
+
+    :param basis_set_filepaths: Full paths to basis set files.
+    :type basis_set_filepaths: list
+
+    :param sym2Z: Dictionary associating atomic symbols to atomic numbers
+    :type sym2Z: dict
+
+    :param l2num: Lambda function associating orbital letters with a number
+    :type l2num: lambda
+
+    :return: Collection of basis sets and the supported elements of each.
+    :rtype: dict
+    """
+
+    atom_shell_line = re.compile("^[a-zA-Z]{1,3}\s+[a-zA-Z]+\s*$")
+    shell_data = re.compile(
+        "^\s*(?:-?\d+\.\d+(?:(?:E|e|D|d)(?:\+|-)\d\d)*\s*)+$")
+
+    bases = {}
+    for filepath in basis_set_filepaths:
+        # Extract file name without extension
+        basis_set = os.path.splitext(os.path.basename(filepath))[0]
+
+        # Let the user know which basis set is being parsed
+        print("Parsing {}...".format(os.path.basename(filepath)), 
+              end='')
+        # Print immediately
+        sys.stdout.flush()
+
+        # Read data from the file into memory
+        tmp_basis = {}
+        with open(filepath, 'r') as fin:
+            atom_z = 0     # Atomic number of current element
+            ls = ""        # Angular momenta of current shells
+
+            for line in fin:
+                if re.search(atom_shell_line, line):
+                    atom_sym, ls = line.split()
+
+                    # Update current atomic number and angular momentum
+                    atom_z = sym2Z[atom_sym.lower()]
+
+                    # Add the new element to basis set list
+                    if (not atom_z in tmp_basis.keys()):
+                        tmp_basis[atom_z] = {}
+                    if (not ls in tmp_basis[atom_z].keys()):
+                        tmp_basis[atom_z][ls] = []
+                    
+                    tmp_basis[atom_z][ls].append([])
+
+                elif re.search(shell_data, line):
+                    # Build up shell data to generate primitives
+                    tmp_basis[atom_z][ls][-1].append(line.split())
+
+        # Process the data read from the file
+        bases[basis_set] = {}
+
+        for element in tmp_basis.keys():
+            bases[basis_set][element] = []
+
+            for ls in tmp_basis[element].keys():
+                for exp_coefs in tmp_basis[element][ls]:
+
+                    # Transpose the array
+                    exp_coefs_t = list(map(list, zip(*exp_coefs)))
+
+                    rowspan = len(ls)
+
+                    exp = exp_coefs_t[0]
+
+                    coefs = [ row for row in exp_coefs_t[1:] ]
+
+                    for j in range(len(coefs) - rowspan + 1):
+                        shell = Shell([ l2num(l.lower()) for l in ls ])
+
+                        for i in range(len(exp)):
+                            coef = [ row[i] for row in coefs[j:j + rowspan:] ]
+
+                            # No coefficients should be zero
+                            if (all([ float(c) != 0.0 for c in coef ])):
+                                shell.add_prim(exp[i], coef)
+                        
+                        bases[basis_set][element].append(shell)
+
+        # Let the user know which basis set is being parsed
+        print("complete")
+        # Print immediately
+        sys.stdout.flush()
+
+    return bases
+
+def parse_bases(basis_set_filepaths, sym2Z, l2num, format="nwchem"):
+    """Parse basis set files of the specified format.
+
+    :param basis_set_filepaths: Full paths to basis set files.
+    :type basis_set_filepaths: list of str
+
+    :param sym2Z: Mapping from lowercased atomic symbols to atomic numbers
+    :type sym2Z: dict
+
+    :param l2num: Conversion function from shell symbol (s, p, d, f, etc)
+        to the corresponding number (0, 1, 2, 3, etc)
+    :type l2num: lambda
+
+    :param format: File formatting to parse, defaults to "nwchem"
+    :type format: str, optional
+
+    :raises RuntimeError: Unsupported basis file format.
+    """
+
+    if (format == "gaussian94" or 
+        format == "psi4"       or
+        format == "xtron"):
+        
+        return parse_bases_gbs(basis_set_filepaths, sym2Z, l2num)
+    elif (format == "nwchem"):
+        return parse_bases_nw(basis_set_filepaths, sym2Z, l2num)
+    else:
+        raise RuntimeError("Unsupported basis file format: {}".format(format))
+
+def find_basis_sets(source_root, formats=["nwchem"], recursive=False):
+    """Recursively find all basis set files in the given directory. Basis set
+    files are identified using the given extensions list.
+
+    :param source_root: Root directory containing basis set files.
+    :type source_root: str
+    
+    :param extensions: Possible extensions for basis sets, defaults to [".nw"]
+    :type extensions: list, optional
+
+    :param recursive: Whether or not to recursively search in subdirectories,
+        defaults to False
+    :type recursive: bool
+    
+    :return: Full paths to basis set files found
+    :rtype: list
+    """
+    basis_sets = []
+
+    for format in formats:
+        extension = helpers.lookup_extension(format)
+
+        # Recursively search for basis set files of the given extension
+        for dirpath, _, filenames in os.walk(source_root):
+            for filename in filenames:
+                _, ext = os.path.splitext(filename)
+
+                # Case insensitive comparison of extensions
+                if (ext.lower() == extension.lower()):
+                    basis_sets.append(os.path.join(dirpath, filename))
+
+            # Stop the recursive search before diving into subdirectories
+            # if recursion is not requested
+            if (not recursive):
+                break
+
+    return basis_sets
+
 def main(args):
+    """Entry point function to generate basis set files.
 
-    basis_sets = [f.replace(".gbs","") for f in os.listdir("basis_sets/default") if os.path.isfile(os.path.join("basis_sets","default",f))]
-    my_dir = os.path.dirname(os.path.realpath(__file__))
-    out_dir = os.path.join(os.path.dirname(my_dir), "libchemist",
-                           "defaults")
-    out_dir   = os.path.abspath(args.destination)
+    :param args: Command line argument namespace
+    :type args: Namespace
+    """
+
+    formats = [ "nwchem" ]
+
+    # Set include directory to default src_dir path if no path is specified
+    if (args.inc == ""):
+        args.inc = args.src_dir
+
+    # Create some paths
+    my_dir    = os.path.dirname(os.path.realpath(__file__))
+    inc_dir   = os.path.abspath(args.inc)
+    src_dir   = os.path.abspath(args.src_dir)
     test_dir  = os.path.abspath(args.test_path)
-    atoms = parse_symbols(os.path.join(my_dir, "physical_data",
-                                       "ElementNames.txt"), {})
-    sym2Z = {ai.sym.lower() : ai.Z for ai in atoms.values()}
-    l2num = lambda l: "spdfghijklmnoqrtuvwxyzabce".find(l.lower())
-    bases = parse_bases(basis_sets, sym2Z, l2num)
-    write_bases(out_dir, bases)
+    name_file = os.path.join(my_dir, "physical_data", "ElementNames.txt")
 
+    # Discover basis set files
+    basis_set_dir       = os.path.abspath(args.basis_set_source)
+    basis_set_filepaths = find_basis_sets(basis_set_dir, 
+                                          formats=formats,
+                                          recursive=args.recursive)
+
+    # Parse element information
+    atoms = {}
+    parse_symbols(name_file, atoms)
+
+    sym2Z = { ai.sym.lower() : ai.Z for ai in atoms.values() }
+    l2num = lambda l: "spdfghijklmnoqrtuvwxyzabce".find(l.lower())
+
+    # If more than one file format is being parsed, the basis_set_filepaths
+    # list could be filtered by file extension and each format parsed
+    # separately by separate calls to parse_bases. File extensions for formats
+    # can be looked up with helpers.lookup_extension(format)
+    bases = parse_bases(basis_set_filepaths, sym2Z, l2num, format=formats[0])
+
+    write_bases(inc_dir, src_dir, bases)
+
+def parse_args():
+    """Parse command line arguments.
+
+    :return: Values of command line arguments.
+    :rtype: Namespace
+    """
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('basis_set_source', type=str,
+                        help="""Source directory for basis set files. If combined
+                             with the \"-r\" flag, this directory will be
+                             recursively searched for basis sets.""")
+    parser.add_argument('src_dir', type=str,
+                        help="Destination directory for generated source files.")
+    parser.add_argument('test_path', type=str,
+                        help="Destination directory for generated unit tests.")
+
+    parser.add_argument('-i', '--inc', type=str,
+                        default="",
+                        help="""Destination include directory, if different 
+                             than the required \"destination\" argument.""")
+    parser.add_argument('-r', '--recursive', action="store_true",
+                        help="""Toggle on recursive search through the basis 
+                             set source directory. Default OFF.""")
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
+    args = parse_args()
+
     main(args)
