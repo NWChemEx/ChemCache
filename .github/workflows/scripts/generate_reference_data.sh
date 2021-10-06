2ec7bccb2c149acb130c0dbfc355fe93ec50f2db
zachcran <zachcran@iastate.edu>
2021-10-06 17:50:57 +0000

    Add script to run data generation scripts
    * Added script to call all data generation scripts
    * For consistency, molecule files were also moved to reference_data/molecules/default

---
+++ .github/workflows/scripts/generate_reference_data.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# This script generates the reference data source code.
+#
+# Usage:
+#   generate_reference_data.sh
+#
+# Notes:
+#   - This script assumes Sphinx is installed via PIP into a virtual environment
+#     called "venv"
+
+set -e # Exit with error if any command fails
+
+# Set some environment-specific values
+PYTHON="python"
+
+# Set reference data directory
+REFERENCE_DATA="reference_data"
+
+# Set source code output directory
+SRC="src/chemcache"
+
+# Set data directories
+ATOMIC_INFO="${REFERENCE_DATA}/physical_data"
+BASIS_SETS="${REFERENCE_DATA}/basis_sets"
+DENSITIES="${REFERENCE_DATA}/atomic_densities"
+MOLECULES="${REFERENCE_DATA}/molecules"
+
+# Create necessary src subdirectories
+mkdir -p "${SRC}/atomic_densities"
+mkdir -p "${SRC}/bases"
+
+# Activate virtual environment
+. venv/bin/activate
+
+# Call generation scripts
+echo "Calling ${REFERENCE_DATA}/generate_atomicinfo.py"
+${PYTHON} ${REFERENCE_DATA}/generate_atomicinfo.py ${ATOMIC_INFO} ${SRC}
+
+# echo "Calling ${REFERENCE_DATA}/generate_densities.py"
+# ${PYTHON} ${REFERENCE_DATA}/generate_densities.py ${DENSITIES} -i ${INC} ${SRC} ${TESTS} -r
+
+echo "Calling ${REFERENCE_DATA}/generate_molecules.py"
+${PYTHON} ${REFERENCE_DATA}/generate_molecules.py ${MOLECULES} ${SRC} -r
+
+echo "Calling ${REFERENCE_DATA}/generate_basis.py"
+${PYTHON} ${REFERENCE_DATA}/generate_basis.py ${BASIS_SETS} ${SRC} -r
