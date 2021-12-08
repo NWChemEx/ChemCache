e60b5cad1083fab696b0c629c39fc04b4453f94c
Jonathan M. Waldrop <relik107@gmail.com>
2021-12-08 16:12:47 -0600

    AtomicBasisSet update for generate_basis.py.

--- reference_data/generate_basis.py
+++ reference_data/generate_basis.py
@@ -117,7 +117,7 @@ def print_atom_basis(fout: io.TextIOWrapper, bs_name: str, z: int,
     """
 
     fout.write("{}basis_map.emplace({}, ".format(tab, z))
-    fout.write("chemist::AtomicBasisSet<double>(0.0, 0.0, 0.0));\n\n")
+    fout.write("chemist::AtomicBasisSet<double>(\"{}\", {}, 0.0, 0.0, 0.0));\n\n".format(bs_name, z))
 
     center = "basis_map.at({})".format(z)
 
