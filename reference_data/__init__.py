708100d9d508cb64749ee26818c7eae44a8f746c
zachcran <zachcran@iastate.edu>
2022-02-09 18:27:55 -0600

    Initial top-level package documentation [no ci]

---
+++ reference_data/__init__.py
@@ -0,0 +1,5 @@
+"""The ``reference_data`` package contains scripts and reference data 
+needed to generate C++-accessible versions of the reference data, 
+currently in the form of C++ source code. Some of the reference data is 
+downloaded, and those download scripts are also contained here.
+"""
