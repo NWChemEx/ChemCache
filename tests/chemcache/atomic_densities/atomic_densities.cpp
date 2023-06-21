5d8c2de4b63bebd4982a0228945b2b3e8c865147
Kevin Gasperich <kgasperich@gmail.com>
2023-06-21 15:55:17 -0500

    updated atom dm test

--- tests/chemcache/atomic_densities/atomic_densities.cpp
+++ tests/chemcache/atomic_densities/atomic_densities.cpp
@@ -18,8 +18,10 @@
 #include <catch2/catch.hpp>
 #include <simde/simde.hpp>
 
-using atomic_den_pt = simde::AtomDenFromZ;
-using atomic_den_t  = std::vector<double>;
+using atomic_basis_pt  = simde::AtomicBasisSetFromZ;
+using atomic_basis_t   = simde::type::atomic_basis_set;
+using atomic_den_ao_pt = simde::AtomDenAOFromZ;
+using atomic_den_t     = std::vector<double>;
 
 using Catch::Matchers::Message;
 
@@ -29,25 +31,50 @@ TEST_CASE("Atomic Densities") {
     auto& atom_dm_mod = mm.at("sto-3g atomic dm");
 
     SECTION("sto-3g Hydrogen") {
-        auto rv = atom_dm_mod.run_as<atomic_den_pt>(1ul);
-        atomic_den_t corr{1.00000000};
-        REQUIRE(rv == corr);
+        auto [atom_basis, rho_flat] = atom_dm_mod.run_as<atomic_den_ao_pt>(1ul);
+        atomic_den_t corr_rho{1.00000000};
+        atomic_basis_t corr_bs("sto-3g", 1, 0.0, 0.0, 0.0);
+        corr_bs.add_shell(chemist::ShellType::pure, 0,
+                       std::vector<double>{1.5432896730e-01, 5.3532814230e-01,
+                                           4.4463454220e-01},
+                       std::vector<double>{3.4252509140e+00, 6.2391372980e-01,
+                                           1.6885540400e-01});
+
+        REQUIRE(rho_flat == corr_rho);
+        REQUIRE(atom_basis == corr_bs);
     }
 
     SECTION("sto-3g Oxygen") {
-        auto rv = atom_dm_mod.run_as<atomic_den_pt>(8ul);
-        atomic_den_t corr{
+        auto [atom_basis, rho_flat] = atom_dm_mod.run_as<atomic_den_ao_pt>(8ul);
+        atomic_den_t corr_rho{
           2.11870860,  -0.50150667, 0.00000000, 0.00000000, 0.00000000,
           -0.50150667, 2.11870860,  0.00000000, 0.00000000, 0.00000000,
           0.00000000,  0.00000000,  1.33333333, 0.00000000, 0.00000000,
           0.00000000,  0.00000000,  0.00000000, 1.33333333, 0.00000000,
           0.00000000,  0.00000000,  0.00000000, 0.00000000, 1.33333333};
-        REQUIRE(rv == corr);
+        atomic_basis_t corr_bs("sto-3g", 8, 0.0, 0.0, 0.0);
+        corr_bs.add_shell(chemist::ShellType::pure, 0,
+                       std::vector<double>{1.5432896730e-01, 5.3532814230e-01,
+                                           4.4463454220e-01},
+                       std::vector<double>{1.3070932140e+02, 2.3808866050e+01,
+                                           6.4436083130e+00});
+        corr_bs.add_shell(chemist::ShellType::pure, 0,
+                       std::vector<double>{-9.9967229190e-02, 3.9951282610e-01,
+                                           7.0011546890e-01},
+                       std::vector<double>{5.0331513190e+00, 1.1695961250e+00,
+                                           3.8038896000e-01});
+        corr_bs.add_shell(chemist::ShellType::pure, 1,
+                       std::vector<double>{1.5591627500e-01, 6.0768371860e-01,
+                                           3.9195739310e-01},
+                       std::vector<double>{5.0331513190e+00, 1.1695961250e+00,
+                                           3.8038896000e-01});
+        REQUIRE(rho_flat == corr_rho);
+        REQUIRE(atom_basis == corr_bs);
     }
 
     SECTION("Out of Range") {
-        REQUIRE_THROWS_MATCHES(atom_dm_mod.run_as<atomic_den_pt>(1000ul),
+        REQUIRE_THROWS_MATCHES(atom_dm_mod.run_as<atomic_den_ao_pt>(1000ul),
                                std::out_of_range,
-                               Message("Atomic Density not available for Z"));
+                               Message("Basis Set not available for Z"));
     }
 }
