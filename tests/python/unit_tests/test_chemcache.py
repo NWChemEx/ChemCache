f6d197d325aceae985ba66017430aba7294010ed
Ryan Richard <ryanmrichard1@gmail.com>
2023-06-21 14:29:48 -0500

    Python (#57)
    * try 1
    
    * works with python now
    
    * Committing license headers
    
    ---------
    
    Co-authored-by: license[bot] <license[bot]@github.com>

--- tests/python/unit_tests/test_chemcache.py
+++ tests/python/unit_tests/test_chemcache.py
@@ -1,4 +1,5 @@
-# Copyright 2022 NWChemEx-Project
+#
+# Copyright 2023 NWChemEx-Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,10 +12,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-import sys
 import os
+import parallelzone as pz
+import sys
+import unittest
 
-sys.path.append(
-    os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                 "../reference_data"))
+
+
+if __name__ == '__main__':
+    rv = pz.runtime.RuntimeView()
+
+    my_dir = os.path.dirname(os.path.realpath(__file__))
+
+    loader = unittest.TestLoader()
+    tests  = loader.discover(my_dir)
+    testrunner = unittest.runner.TextTestRunner()
+    ret = not testrunner.run(tests).wasSuccessful()
+    sys.exit(ret)
