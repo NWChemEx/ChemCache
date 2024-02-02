1aabd7dd744189ecb99b4c405d5649b93884c2aa
Jonathan M. Waldrop <relik107@gmail.com>
2024-02-02 15:44:43 -0600

    update CI data scripts

--- .github/workflows/scripts/generate_reference_data.sh
+++ .github/workflows/scripts/generate_reference_data.sh
@@ -11,57 +11,39 @@
 
 set -e # Exit with error if any command fails
 
-# Set some environment-specific values
-PYTHON="python"
-
-# Set reference data directory
-REF_DATA="reference_data"
-
-# Set source code output directories
-SRC="src/chemcache"
-EXP_SRC="experimental/src/chemcache"
-ATOM_DEN="${EXP_SRC}/atomic_densities"
-ATOM="${SRC}/atoms"
-BASES="${SRC}/bases"
-ELEC_CONFIGS="${EXP_SRC}/electronic_configurations"
-MOLES="${SRC}/molecules"
-
 # Set data directories
+REF_DATA="reference_data"
 ATOMIC_INFO="${REF_DATA}/physical_data"
 BASIS_SETS="${REF_DATA}/basis_sets"
 DENSITIES="${REF_DATA}/atomic_densities/default"
 MOLECULES="${REF_DATA}/molecules"
-TESTS="tests/chemcache"
 
-# Remove existing source files
-# rm -rf $ATOM_DEN
-# rm -rf $ATOM
-# rm -rf $BASES
-# rm -rf $ELEC_CONFIGS
-# rm -rf $MOLES
-
-# Recreate necessary src subdirectories
-# mkdir -p $ATOM_DEN
-# mkdir -p $ATOM
-# mkdir -p $BASES
-# mkdir -p $ELEC_CONFIGS
-# mkdir -p $MOLES
+# Set source code output directories
+SRC="src/chemcache"
+ATOM="${SRC}/atoms"
+BASES="${SRC}/bases"
+MOLES="${SRC}/molecules"
+EXP_SRC="experimental/src/chemcache"
+ATOM_DEN="${EXP_SRC}/atomic_densities"
+ELEC_CONFIGS="${EXP_SRC}/electronic_configurations"
 
 # Activate virtual environment
 . venv/bin/activate
 
-# Call generation scripts
-echo "Calling ${REF_DATA}/generate_atomicinfo.py ${ATOMIC_INFO} ${ATOM}"
-${PYTHON} ${REF_DATA}/generate_atomicinfo.py ${ATOMIC_INFO} ${ATOM}
+# Call generation script(s)
+SCRIPTS_DIR="utils/data_management"
 
-echo "Calling ${REF_DATA}/generate_densities.py ${DENSITIES} ${ATOM_DEN} -r" 
-${PYTHON} ${REF_DATA}/generate_densities.py ${DENSITIES} ${ATOM_DEN} -r
+echo "Calling ${SCRIPTS_DIR}/generate_atomicinfo.py ${ATOMIC_INFO} ${ATOM}"
+python ${SCRIPTS_DIR}/generate_atomicinfo.py ${ATOMIC_INFO} ${ATOM}
 
-echo "Calling ${REF_DATA}/generate_molecules.py ${MOLECULES} ${MOLES} -r"
-${PYTHON} ${REF_DATA}/generate_molecules.py ${MOLECULES} ${MOLES} -r
+echo "Calling ${SCRIPTS_DIR}/generate_basis.py ${BASIS_SETS} ${BASES} -r"
+python ${SCRIPTS_DIR}/generate_basis.py ${BASIS_SETS} ${BASES} -r
 
-echo "Calling ${REF_DATA}/generate_basis.py ${BASIS_SETS} ${BASES} -r"
-${PYTHON} ${REF_DATA}/generate_basis.py ${BASIS_SETS} ${BASES} -r
+echo "Calling ${SCRIPTS_DIR}/generate_molecules.py ${MOLECULES} ${MOLES} -r"
+python ${SCRIPTS_DIR}/generate_molecules.py ${MOLECULES} ${MOLES} -r
 
-echo "Calling ${REF_DATA}/generate_elec_configs.py ${ATOMIC_INFO} ${ELEC_CONFIGS}"
-${PYTHON} ${REF_DATA}/generate_elec_configs.py ${ATOMIC_INFO} ${ELEC_CONFIGS}
+echo "Calling ${SCRIPTS_DIR}/generate_densities.py ${DENSITIES} ${ATOM_DEN} -r" 
+python ${SCRIPTS_DIR}/generate_densities.py ${DENSITIES} ${ATOM_DEN} -r
+
+echo "Calling ${SCRIPTS_DIR}/generate_elec_configs.py ${ATOMIC_INFO} ${ELEC_CONFIGS}"
+python ${SCRIPTS_DIR}/generate_elec_configs.py ${ATOMIC_INFO} ${ELEC_CONFIGS}
